284e00096f80dee53e8de7be08629e59e5491359
Chetan Kaushik <dynamitechetan@gmail.com>
2017-04-11 22:34:35 +0530

    adding live score Endpoint

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 # data conversion, the headers for the response, etc
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 import json
 import sys
 from bs4 import BeautifulSoup
-from cric import Cricbuzz
+from cricbuzz import Cricbuzz
 
 # Initialize the Flask application
 app = Flask(__name__)
-
+app.run(debug=True)
 
 # This route will return a list in JSON format
-@app.route('/')
-def index():
+@app.route('/getMatches')
+def getMatches():
     # This is a dummy list, 2 nested arrays containing some
     # params and values
     c = Cricbuzz()
@@ -22,6 +22,18 @@ def index():
     # jsonify will do for us all the work, returning the
     # previous data structure in JSON
     return jsonify(results=list)
+
+@app.route('/liveScore')
+def getMatchInfo():
+    matchId = request.args.get('matchId')
+    c = Cricbuzz()
+    # jsonify will do for us all the work, returning the
+    # previous data structure in JSON
+    list =  c.livescore(matchId)
+    # jsonify will do for us all the work, returning the
+    # previous data structure in JSON
+    return jsonify(results=list)
+
 
 
 if __name__ == '__main__':
